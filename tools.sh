4bbe4e464565b958567861254e8b9d140ec12aeb
Sacha Wharton <32266273+sachajw@users.noreply.github.com>
2024-12-11 21:07:45 +0200

    🛠 NEW: tweaks

--- tools.sh
+++ tools.sh
@@ -18,7 +18,7 @@ apt-get update && apt-get upgrade -y
 # Install essential packages
 echo "Installing essential packages..."
 apt-get install -y python3 python3-dev maven git-all podman \
-    ca-certificates curl gnupg lsb-release unzip
+    ca-certificates curl gnupg lsb-release unzip zip
 
 # Install SDKMAN and Java
 echo "Installing SDKMAN and Java..."
@@ -43,27 +43,33 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Enable Docker service
-# systemctl enable docker
-# systemctl start docker
+systemctl enable docker
+systemctl start docker
 
 # Install Docker Credential Helper for GCR
 echo "Installing Docker Credential Helper for GCR..."
-curl -L -o docker-credential-gcr.tar.gz https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v2.1.1/docker-credential-gcr_linux_amd64-2.1.1.tar.gz
+curl -L -o docker-credential-gcr.tar.gz https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/latest/download/docker-credential-gcr_linux_amd64.tar.gz
 tar -xvzf docker-credential-gcr.tar.gz -C /usr/local/bin
 
 # Install yj
 echo "Installing yj..."
-curl -L -o /usr/local/bin/yj https://github.com/sclevine/yj/releases/download/v5.1.0/yj-linux-amd64
-chmod +x /usr/local/bin/yj
+if ! command -v yj &> /dev/null || [[ "$(yj --version)" != "5.1.0" ]]; then
+    curl -L -o /usr/local/bin/yj https://github.com/sclevine/yj/releases/latest/download/yj-linux-amd64
+    chmod +x /usr/local/bin/yj
+else
+    echo "yj is already up to date."
+fi
 
 # Install yq
 echo "Installing yq..."
-curl -L -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.24.5/yq_linux_amd64
+LATEST_YQ_VERSION=$(curl -s https://api.github.com/repos/mikefarah/yq/releases/latest | grep 'tag_name' | cut -d '"' -f 4)
+curl -L -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/${LATEST_YQ_VERSION}/yq_linux_amd64
 chmod +x /usr/local/bin/yq
 
 # Install ytt
 echo "Installing ytt..."
-curl -L -o /usr/local/bin/ytt https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.40.1/ytt-linux-amd64
+LATEST_YTT_VERSION=$(curl -s https://api.github.com/repos/vmware-tanzu/carvel-ytt/releases/latest | grep 'tag_name' | cut -d '"' -f 4)
+curl -L -o /usr/local/bin/ytt https://github.com/vmware-tanzu/carvel-ytt/releases/download/${LATEST_YTT_VERSION}/ytt-linux-amd64
 chmod +x /usr/local/bin/ytt
 
 # Final system cleanup
