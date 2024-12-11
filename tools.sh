12fc01185790596be08b945677f882a4bfcf3e26
Sacha Wharton <32266273+sachajw@users.noreply.github.com>
2024-12-11 20:57:05 +0200

    🛠 NEW: tweaks

--- tools.sh
+++ tools.sh
@@ -10,34 +10,27 @@ mkdir -p "$LOCAL_DIR"
 echo 'export PATH="$HOME/.local:$PATH"' >> "$BASHRC"
 
 # Update and upgrade system packages
-echo "Updating and upgrading Ubuntu packages..."
+echo "Updating and upgrading Ubuntu packages using a local mirror..."
+sed -i.bak -e 's|http://archive.ubuntu.com/ubuntu/|http://<mirror-url>/ubuntu/|g' /etc/apt/sources.list
+echo "Switched to local mirror for faster package downloads."
 apt-get update && apt-get upgrade -y
 
-# Install curl
-echo "Installing Curl..."
-apt-get install curl -y
-
-# Install Python3 and development tools
-echo "Installing Python3..."
-apt-get install -y python3 python3-dev
+# Install essential packages
+echo "Installing essential packages..."
+apt-get install -y python3 python3-dev maven git-all podman \
+    ca-certificates curl gnupg lsb-release unzip
 
 # Install SDKMAN and Java
 echo "Installing SDKMAN and Java..."
-curl -s "https://get.sdkman.io" | bash
+if [ ! -d "$HOME/.sdkman" ]; then
+    curl -s "https://get.sdkman.io" | bash
+else
+    echo "SDKMAN is already installed. Skipping installation."
+fi
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java
 sdk env init && sdk env
 java --version
-
-# Install Maven
-echo "Installing Maven..."
-apt-get install -y maven
-mvn -version
-
-# Install Git
-echo "Installing Git..."
-apt-get install -y git-all
-git --version
 
 # Install Git Large File Storage (LFS)
 echo "Installing Git LFS..."
@@ -45,10 +38,13 @@ curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.s
 
 # Install Docker
 echo "Installing Docker..."
-apt-get install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Enable Docker service
+# systemctl enable docker
+# systemctl start docker
 
 # Install Docker Credential Helper for GCR
 echo "Installing Docker Credential Helper for GCR..."
